558018c51df198e4c4e6f9825c3a7ff1d99eb6b8
Stefan Matzner <stefanmatzner@gmx.at>
2025-10-20 20:01:30 +0200

    feat: update index.html and main.ts for Task Pane integration and adjust global styles

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { bootstrapApplication } from '@angular/platform-browser'
+import { TaskPaneComponent } from './app/task-pane/task-pane.component'
+import { appConfig } from './app/app.config'
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+if (typeof Office !== 'undefined') {
+  Office.onReady().then(() => {
+    bootstrapApplication(TaskPaneComponent, appConfig)
+  })
+} else {
+  bootstrapApplication(TaskPaneComponent, appConfig)
+}
