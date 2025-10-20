55a31224861f8bbb98f981872a6a1a018b60b7a4
Stefan Matzner <stefanmatzner@gmx.at>
2025-10-20 20:00:39 +0200

    build(deps): integrate TailwindCSS and PrimeNG themes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,34 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {providePrimeNG} from 'primeng/config'
+import Aura from '@primeng/themes/aura';
+import {definePreset} from '@primeuix/themes'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: definePreset(Aura, {
+          semantic: {
+            primary: {
+              50: '{noir.50}',
+              100: '{noir.100}',
+              200: '{noir.200}',
+              300: '{noir.300}',
+              400: '{noir.400}',
+              500: '{noir.500}',
+              600: '{noir.600}',
+              700: '{noir.700}',
+              800: '{noir.800}',
+              900: '{noir.900}',
+              950: '{noir.950}'
+            }
+          }
+        })
+      }
+    })
+  ]
 };
