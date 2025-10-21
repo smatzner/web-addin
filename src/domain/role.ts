d81c5f1697a91a328da298f88999a10ef90bf9c6
Stefan Matzner <stefanmatzner@gmx.at>
2025-10-21 14:56:43 +0200

    feat(RoleService): remove color property from role data structure

--- src/domain/role.ts
+++ src/domain/role.ts
@@ -1,5 +1,4 @@
 export interface Role {
   id: number;
   name: string;
-  color: string;
 }
--- src/service/role.service.ts
+++ src/service/role.service.ts
@@ -7,11 +7,11 @@ import {Role} from '../domain/role'
 export class RoleService {
   getRoleData() : Role[] {
     return [
-      { id: 1, name: 'Außer Kraft', color: '#000' },
-      { id: 2, name: 'Betrieblicher Wagendienst', color: '#000' },
-      { id: 3, name: 'Betriebsassistent', color: '#000' },
-      { id: 4, name: 'Betriebskoordination', color: '#000' },
-      { id: 5, name: 'Betriebskoordinator', color: '#000' },
+      { id: 1, name: 'Außer Kraft' },
+      { id: 2, name: 'Betrieblicher Wagendienst' },
+      { id: 3, name: 'Betriebsassistent' },
+      { id: 4, name: 'Betriebskoordination' },
+      { id: 5, name: 'Betriebskoordinator' },
     ];
   }
 }
